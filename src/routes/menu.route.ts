3bdfffc697ef0d9dfbc3669b2ea0cd9ae5de8d69
Reza Aditama <aditamareza03@gmail.com>
2026-04-18 10:07:07 +0700

    fixed bug

--- src/routes/menu.route.ts
+++ src/routes/menu.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { authenticateToken } from "../middlewares/auth.middleware";
 import { authorizeRole } from "../middlewares/role.middleware";
-import { createMenuController, getAllMenuController, getMenuByIdController, updateMenuController } from "../services/menu/menu.controller";
+import { createMenuController, deleteMenuController, getAllMenuController, getMenuByIdController, updateMenuController } from "../services/menu/menu.controller";
 import { uploadMenuImage } from "../middlewares/upload.middleware";
 
 const MenuRouter = Router();
@@ -10,5 +10,6 @@ MenuRouter.get("/", getAllMenuController);
 MenuRouter.get("/:id", getMenuByIdController);
 MenuRouter.post("/create-menu", authenticateToken, authorizeRole(["CASHIER"]), uploadMenuImage.single("image_path"), createMenuController);
 MenuRouter.patch("/update-menu/:id", authenticateToken, authorizeRole(["CASHIER"]), updateMenuController);
+MenuRouter.delete("/delete-menu/:id", authenticateToken, authorizeRole(["CASHIER"]), deleteMenuController)
 
 export default MenuRouter;
