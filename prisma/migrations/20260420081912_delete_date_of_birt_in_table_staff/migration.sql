2ad007ec91fb9cc999cfc116d0a40afd3c399ac7
Aldi Maldi <taktau534@gmail.com>
2026-05-01 13:52:40 +0700

    Fixed schema

---
+++ prisma/migrations/20260420081912_delete_date_of_birt_in_table_staff/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `date_of_birth` on the `staff` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "staff" DROP COLUMN "date_of_birth";
